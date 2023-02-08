2002a2cba80b14ab593ab8fb0fa8ecbfd875bdd8
paresiqbal <thecrowdel@gmail.com>
2023-02-08 08:15:44 +0700

    "FIx ui"

--- app/page.js
+++ app/page.js
@@ -2,16 +2,29 @@ import Link from "next/link";
 
 export default function Home() {
   return (
-    <div>
-      <h1 className="text-3xl font-semibold text-center">Next 13 Game's</h1>
-      <div className="flex flex-col gap-4 p-24 text-white text-2xl">
-        <div className=" bg-gray-800 shadow-lg rounded-lg text-center py-10">
-          <h1 className="pb-2">Tic Tac Toe Game</h1>
+    <div className="p-10 bg-gray-900 h-screen">
+      <h1 className="text-4xl font-bold text-center text-white">
+        Next 13 Game's
+      </h1>
+      <div className="flex flex-row gap-4 p-24 text-white text-2xl">
+        <div className=" bg-gray-800 hover:bg-gray-700  shadow-lg rounded-lg text-center py-10 px-24">
+          <h1 className="pb-4">Tic Tac Toe Game</h1>
+          <h2 className="pb-4">O | X</h2>
           <Link
             href={"/TicTacToe"}
-            className="py-1 px-6 bg-cyan-500 rounded-lg"
+            className="py-1 px-6 bg-cyan-600 hover:bg-cyan-500 rounded-lg cursor-pointer "
           >
-            Go
+            Play game
+          </Link>
+        </div>
+        <div className=" bg-gray-800 hover:bg-gray-700  shadow-lg rounded-lg text-center py-10 px-10">
+          <h1 className="pb-4">Rock Paper Scissors Game</h1>
+          <h2 className="pb-4">O | X</h2>
+          <Link
+            href={"/TicTacToe"}
+            className="py-1 px-6 bg-cyan-600 hover:bg-cyan-500 rounded-lg cursor-pointer "
+          >
+            Play game
           </Link>
         </div>
       </div>
