8302416a851587f06c70a7e3613d5d379d3201bb
paresiqbal <thecrowdel@gmail.com>
2023-02-08 10:15:54 +0700

    "Adding react icons"

--- app/page.js
+++ app/page.js
@@ -1,4 +1,6 @@
 import Link from "next/link";
+import { BsCircle } from "react-icons/bs";
+import { FaTimes } from "react-icons/fa";
 
 export default function Home() {
   return (
@@ -8,8 +10,11 @@ export default function Home() {
       </h1>
       <div className="flex flex-row gap-4 p-24 text-white text-2xl">
         <div className=" bg-gray-800 hover:bg-gray-700  shadow-lg rounded-lg text-center py-10 px-24">
-          <h1 className="pb-4">Tic Tac Toe Game</h1>
-          <h2 className="pb-4">O | X</h2>
+          <h1 className="font-bold">Tic Tac Toe Game</h1>
+          <h2 className="flex gap-4 justify-center items-center py-5 text-3xl">
+            <BsCircle className="text-red-500" />
+            <FaTimes className="text-cyan-500" />
+          </h2>
           <Link
             href={"/TicTacToe"}
             className="py-1 px-6 bg-cyan-600 hover:bg-cyan-500 rounded-lg cursor-pointer "
@@ -18,8 +23,10 @@ export default function Home() {
           </Link>
         </div>
         <div className=" bg-gray-800 hover:bg-gray-700  shadow-lg rounded-lg text-center py-10 px-10">
-          <h1 className="pb-4">Rock Paper Scissors Game</h1>
-          <h2 className="pb-4">O | X</h2>
+          <h1 className="font-bold">Rock Paper Scissors Game</h1>
+          <h2 className="flex gap-4 justify-center items-center py-5 text-3xl">
+            <img src="/rockpaperscissors.png" className="w-14" />
+          </h2>
           <Link
             href={"/RockPaperScissors"}
             className="py-1 px-6 bg-cyan-600 hover:bg-cyan-500 rounded-lg cursor-pointer "
