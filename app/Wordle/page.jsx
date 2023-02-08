0b37f3d706ae8796d33271bbefb03c7d2ed19bf6
paresiqbal <thecrowdel@gmail.com>
2023-02-08 18:37:26 +0700

    "Adding"

---
+++ app/Wordle/page.jsx
@@ -0,0 +1,23 @@
+"use client";
+import { useEffect, useState } from "react";
+
+export default function Wordle() {
+  const [solution, setSolution] = useState(null);
+
+  useEffect(() => {
+    fetch("http://localhost:3001/solutions")
+      .then((res) => res.json())
+      .then((json) => {
+        //Generate random number 0 & 14
+        const randomSolution = json[Math.floor(Math.random() * json.length)];
+        setSolution(randomSolution.word);
+      });
+  }, [setSolution]);
+
+  return (
+    <div>
+      <h1>Worlde</h1>
+      {solution && <div>Solution is {solution}</div>}
+    </div>
+  );
+}
