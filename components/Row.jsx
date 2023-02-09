27c3be9bfa68f4eb691fcd5418cdd12b8784c35a
paresiqbal <thecrowdel@gmail.com>
2023-02-09 11:12:47 +0700

    "adding new feature"

--- components/Row.jsx
+++ components/Row.jsx
@@ -1,4 +1,4 @@
-export default function Row({ guess }) {
+export default function Row({ guess, currentGuess }) {
   if (guess) {
     return (
       <div className="row past">
@@ -6,6 +6,23 @@ export default function Row({ guess }) {
           <div key={i} className={l.color}>
             {l.key}
           </div>
+        ))}
+      </div>
+    );
+  }
+
+  if (currentGuess) {
+    let letters = currentGuess.split("");
+
+    return (
+      <div className="row current">
+        {letters.map((letter, i) => (
+          <div key={i} className="filled">
+            {letter}
+          </div>
+        ))}
+        {[...Array(5 - letters.length)].map((_, i) => (
+          <div key={i}></div>
         ))}
       </div>
     );
