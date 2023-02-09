78bcc1a58d975d612a3262077cbd0ba5c7577ab4
paresiqbal <thecrowdel@gmail.com>
2023-02-09 21:20:40 +0700

    "Adding modal and styling"

--- components/Wordle.jsx
+++ components/Wordle.jsx
@@ -1,19 +1,32 @@
 import useWordle from "@/hooks/useWordle";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import Grid from "./Grid";
 import "../app/index.css";
 import Link from "next/link";
 import Keypad from "./Keypad";
+import Modal from "./Modal";
 
 export default function Worlde({ solution }) {
   const { currentGuess, guesses, turn, isCorrect, usedKeys, handleKeyup } =
     useWordle(solution);
 
+  const [showModal, setShowModal] = useState(false);
+
   useEffect(() => {
     window.addEventListener("keyup", handleKeyup);
 
+    if (isCorrect) {
+      setTimeout(() => setShowModal(true), 2000);
+      window.removeEventListener("keyup", handleKeyup);
+    }
+
+    if (turn > 5) {
+      setTimeout(() => setShowModal(true), 2000);
+      window.removeEventListener("keyup", handleKeyup);
+    }
+
     return () => window.removeEventListener("keyup", handleKeyup);
-  }, [handleKeyup]);
+  }, [handleKeyup, isCorrect]);
 
   useEffect(() => {
     console.log(guesses, turn, isCorrect);
@@ -25,6 +38,10 @@ export default function Worlde({ solution }) {
       <div>Current Guess - {currentGuess}</div>
       <Grid guesses={guesses} currentGuess={currentGuess} turn={turn} />
       <Keypad usedKeys={usedKeys} />
+      {showModal && (
+        <Modal isCorrect={isCorrect} turn={turn} solution={solution} />
+      )}
+
       <Link
         href={"/"}
         className="bg-cyan-500 py-2 px-4 rounded-lg text-2xl hover:bg-cyan-600"
