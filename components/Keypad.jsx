566356a54825c0655af9fa0f70bce18fd3128dbc
paresiqbal <thecrowdel@gmail.com>
2023-02-09 16:00:02 +0700

    "adding keyboard"

---
+++ components/Keypad.jsx
@@ -0,0 +1,11 @@
+import { useState } from "react";
+
+export default function Keypad() {
+  const [letters, setLetters] = useState(null);
+
+  return (
+    <div>
+      <h1></h1>
+    </div>
+  );
+}
